073976132af3ecdef17cada36e10efff3cf70de6
Shubham <shubhamjindal.cse19@jecrc.ac.in>
2020-08-31 16:16:35 +0530

    All CURD operation in Salesforce with LWC

--- CURD/moviesWall/moviesWall.js
+++ CURD/moviesWall/moviesWall.js
@@ -3,6 +3,7 @@ import getDataOfMovies from '@salesforce/apex/movies.getDataOfMovies'
 export default class MoviesWall extends LightningElement {
 @track data =[];
 @track duplicate;
+@track newOne = [];
 @track queryTerm;
 
 async connectedCallback(){
@@ -20,24 +21,44 @@ async connectedCallback(){
         obj.movie_title = await this.duplicate[i]["movie_title"];
         obj.actor_2_name = await this.duplicate[i]["actor_2_name"];
         obj.actor_1_name = await this.duplicate[i]["actor_1_name"];
-
         this.data.push(obj);
     }
+    this.newOne = await this.data;
+    console.log(this.newOne[0].movie_title)
 }
 
-async handleKeyUp(evt) {
+handleKeyUp(evt) {
     const isEnterKey = evt.keyCode === 13;
     if (isEnterKey) {
+        if(evt.target.value.length>=2){
+            console.log("Now I am in If")
         for(let i=0; i<=20; i++){
-             var movie_title = await this.duplicate[i]["movie_title"];
-             console.log(movie_title)
-            if(evt.target.value==movie_title){
+             var dummyName =  this.newOne[i].movie_title;
+             console.log(dummyName)
+             if(evt.target.value==dummyName){
                 this.queryTerm = evt.target.value;
+                this.data = [];
+                let obj = {
+                    "director_name" : "",
+                    "movie_title" : "",
+                    "actor_2_name" : "",
+                    "actor_1_name" : ""
+                }
+                obj.director_name = this.newOne[i]["director_name"];
+                obj.movie_title = this.newOne[i]["movie_title"];
+                obj.actor_2_name = this.newOne[i]["actor_2_name"];
+                obj.actor_1_name = this.newOne[i]["actor_1_name"];
+                this.data.push(obj);
                 break;
-            }else{
-                this.queryTerm = ""
             }
         }
+        }else{
+            console.log("Now I am in else")
+            this.data = this.newOne;
+        }
     }
+
 }
+
+
 }
