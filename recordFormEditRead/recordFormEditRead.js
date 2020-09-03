bb4fface610e3431eda8bc7953e34bab469ec992
Shubham <shubhamjindal.cse19@jecrc.ac.in>
2020-09-03 12:26:56 +0530

    Custom Record Edit form with custom Picklist

--- recordFormEditRead/recordFormEditRead.js
+++ recordFormEditRead/recordFormEditRead.js
@@ -1,30 +1,48 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track } from "lwc";
 
 export default class RecordFormEditRead extends LightningElement {
+  @track firstInputBox = false;
+  @track firstOutput = true;
+  @track updateOne = "John Doe";
 
+  @track updateTwo = "New";
+  @track updateTwoValue = "new";
 
-    @track firstInputBox = false;
-    @track firstOutput = true;
-    @track updateOne = "John Doe"
-    @track updateTwo =  "Marry kom"
-    handle(){
-        this.firstInputBox = true;
-        this.firstOutput = false;
-        // const element = this.template.querySelector('[data-id="text-input-id-1"]');
-        // element.focus();
-    }
+  @track selectedLabel = "New";
+  @track value = "new";
 
+  get options() {
+    return [
+      { label: "New", value: "new" },
+      { label: "In Progress", value: "inProgress" },
+      { label: "Finished", value: "finished" }
+    ];
+  }
 
-    handleSave(){
-         //console.log('ssss')
-         this.updateOne = this.template.querySelector('[data-id="text-input-id-1"]').value;
-         this.updateTwo = this.template.querySelector('[data-id="text-input-id-2"]').value;
-         this.firstInputBox = false;
-         this.firstOutput = true;
-    }
+  handleChange(event) {
+    this.value = event.detail.value;
+    this.selectedLabel = event.target.options.find(
+      (opt) => opt.value === event.detail.value
+    ).label;
+  }
 
-    handleCancel(){
-        this.firstInputBox = false;
-         this.firstOutput = true;
-    }
+  handle() {
+    this.firstInputBox = true;
+    this.firstOutput = false;
+  }
+
+  handleSave() {
+    this.updateOne = this.template.querySelector(
+      '[data-id="text-input-id-1"]'
+    ).value;
+    this.updateTwo = this.selectedLabel;
+    this.updateTwoValue = this.value;
+    this.firstInputBox = false;
+    this.firstOutput = true;
+  }
+
+  handleCancel() {
+    this.firstInputBox = false;
+    this.firstOutput = true;
+  }
 }
